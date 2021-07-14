3494ae160f98f918b8c598cdb8472340caf9d68f
samsour <sam.sauer@hotmail.de>
2021-07-14 13:31:11 +0200

    feat(app): add base css

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <Navigation text="samsour." />
-  <Stage text="Frontend web development" />
-  <Introduction text="I create digital products with a creative mind and the focus on web based applications." />
+  <Stage text="Frontend<br />web development" />
+  <Introduction text="I create digital<br />products with a<br />creative mind<br/>and a focus on<br/>web-based<br />applications." />
   <Social />
   <Footer 
     imprint="imprint"
@@ -23,19 +23,48 @@ import Footer from './components/Footer.vue'
 </script>
 
 <style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  background: transparent;
+  border: 0;
+}
+
 :root {
   --color-white: #fff;
   --color-black: #000;
   --color-primary: var(--color-black);
   --color-secondary: var(--color-white);
+  --h1-size: 9vw;
+  --h2-size: calc(var(--h1-size) / 1.5);
+  --content-spacing-x: 8vw;
+  --content-spacing-y: 4vw;
+  --content-spacing-x--large: 25vw;
+  --content-spacing-y--large: 15vw;
 }
 
-#app {
+main {
   font-family: Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: var(--color-primary);
   background: var(--color-secondary);
 }
+h1, h2, h3, h4 {
+  text-align: left;
+  font-weight: normal;
+  letter-spacing: -0.03em;
+}
+
+h1 {
+  padding: var(--content-spacing-y) var(--content-spacing-x);
+  font-size: var(--h1-size);
+}
+
+h2 {
+  padding: var(--content-spacing-y--large) var(--content-spacing-x--large);
+  font-size: var(--h2-size);
+}
+
 </style>
