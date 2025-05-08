3a62a147526e8e923e8e8d2698177a53810cb1d3
Sam Sauer <sam.sauer@krekeny.com>
2025-05-08 13:41:14 +0200

    feat(ProfileImage): add reveal animation

--- src/components/ProfileImage.vue
+++ src/components/ProfileImage.vue
@@ -6,8 +6,22 @@
            (max-width: 768px) 768px,
            (max-width: 500px) 500px"
     src="../assets/images/avatar.webp"
+    @load="onImageLoad"
+    :class="{ revealed: isLoaded }"
   />
 </template>
+
+<script setup>
+import { ref } from "vue";
+
+const isLoaded = ref(false);
+
+const onImageLoad = () => {
+  requestAnimationFrame(() => {
+    isLoaded.value = true;
+  });
+};
+</script>
 
 <style scoped>
 img {
@@ -15,5 +29,25 @@ img {
   margin-bottom: calc(var(--content-spacing-y--large) / -2);
   margin-left: var(--content-spacing-x--large);
   box-shadow: 0 50px 100px rgba(0, 0, 0, 0.25);
+  mask-image: linear-gradient(to bottom, transparent 0%, transparent 100%);
+  -webkit-mask-image: linear-gradient(
+    to bottom,
+    transparent 0%,
+    transparent 100%
+  );
+  mask-size: 100% 0%;
+  -webkit-mask-size: 100% 0%;
+  mask-position: top;
+  -webkit-mask-position: top;
+  mask-repeat: no-repeat;
+  -webkit-mask-repeat: no-repeat;
+  transition: all 1.2s cubic-bezier(0.4, 0, 0.2, 1);
+}
+
+img.revealed {
+  mask-image: linear-gradient(to bottom, black 0%, black 100%);
+  -webkit-mask-image: linear-gradient(to bottom, black 0%, black 100%);
+  mask-size: 100% 100%;
+  -webkit-mask-size: 100% 100%;
 }
 </style>
