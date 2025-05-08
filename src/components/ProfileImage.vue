6a5b65b3016f262b265b83e39a93f98da321821e
Sam Sauer <sam.sauer@krekeny.com>
2025-05-08 13:46:55 +0200

    feat(ProfileImage): implement GSAP scroll-triggered reveal animation for profile image

--- src/components/ProfileImage.vue
+++ src/components/ProfileImage.vue
@@ -1,26 +1,50 @@
 <template>
   <img
+    ref="imageRef"
     alt="Sam"
     srcset="../assets/images/avatar@2.webp 2x, ../assets/images/avatar.webp 1x"
     sizes="(max-width: 1024px) 1024px,
            (max-width: 768px) 768px,
            (max-width: 500px) 500px"
     src="../assets/images/avatar.webp"
-    @load="onImageLoad"
-    :class="{ revealed: isLoaded }"
   />
 </template>
 
 <script setup>
-import { ref } from "vue";
+import { ref, onMounted } from "vue";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
-const isLoaded = ref(false);
+gsap.registerPlugin(ScrollTrigger);
 
-const onImageLoad = () => {
-  requestAnimationFrame(() => {
-    isLoaded.value = true;
+const imageRef = ref(null);
+
+onMounted(() => {
+  const image = imageRef.value;
+
+  gsap.set(image, {
+    maskImage: "linear-gradient(to bottom, black 0%, black 100%)",
+    WebkitMaskImage: "linear-gradient(to bottom, black 0%, black 100%)",
+    maskSize: "100% 0%",
+    WebkitMaskSize: "0% 0%",
+    maskPosition: "top",
+    WebkitMaskPosition: "top",
+    maskRepeat: "no-repeat",
+    WebkitMaskRepeat: "no-repeat",
   });
-};
+
+  gsap.to(image, {
+    maskSize: "100% 100%",
+    WebkitMaskSize: "100% 100%",
+    duration: 1.5,
+    ease: "expo.out",
+    scrollTrigger: {
+      trigger: image,
+      start: "top bottom-=100",
+      toggleActions: "play none none reverse",
+    },
+  });
+});
 </script>
 
 <style scoped>
@@ -29,25 +53,5 @@ img {
   margin-bottom: calc(var(--content-spacing-y--large) / -2);
   margin-left: var(--content-spacing-x--large);
   box-shadow: 0 50px 100px rgba(0, 0, 0, 0.25);
-  mask-image: linear-gradient(to bottom, transparent 0%, transparent 100%);
-  -webkit-mask-image: linear-gradient(
-    to bottom,
-    transparent 0%,
-    transparent 100%
-  );
-  mask-size: 100% 0%;
-  -webkit-mask-size: 100% 0%;
-  mask-position: top;
-  -webkit-mask-position: top;
-  mask-repeat: no-repeat;
-  -webkit-mask-repeat: no-repeat;
-  transition: all 1.2s cubic-bezier(0.4, 0, 0.2, 1);
-}
-
-img.revealed {
-  mask-image: linear-gradient(to bottom, black 0%, black 100%);
-  -webkit-mask-image: linear-gradient(to bottom, black 0%, black 100%);
-  mask-size: 100% 100%;
-  -webkit-mask-size: 100% 100%;
 }
 </style>
